6f7091779b97b0a8ff57ac50408d36e74239b4dc
mfomniga <44165437+mfomniga@users.noreply.github.com>
2018-10-15 16:41:12 +0200

    ToDo App Aufgabe

---
+++ ToDoApp/ToDoApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using ToDoApp.objects;
+
+namespace ToDoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bitte Namen eingeben");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Schon erledigt? j/n");
+            ConsoleKeyInfo eingabe = Console.ReadKey();
+
+            bool doneumstaendlich;
+            if(eingabe.Key == ConsoleKey.J)
+            {
+                doneumstaendlich = true;
+            }
+            else
+            {
+                doneumstaendlich = false;
+            }
+
+            bool done = eingabe.Key == ConsoleKey.J;            
+
+            ToDo todo = new ToDo(name, done);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ToDoApp/ToDoApp/objects/ToDo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoApp.objects
+{
+    public class ToDo
+    {
+    
+        public ToDo(string name, bool done)
+        {
+            this.Name = name;
+            Console.WriteLine($"Die String Variable Name wurde auf {name} gesetzt.");
+
+            this.Date = DateTime.Now;
+            Console.WriteLine(this.Date);
+            
+            this.Done = done;
+
+            //string variable = Boolean ? wenntrue : wennfalse
+            string jaNein = done ? "ja" : "nein";
+
+            Console.WriteLine($"Schon erledigt? {jaNein}");
+        }
+
+        public string Name { get; private set; }
+        public DateTime Date { get; private set; }
+        public bool Done { get; set; }
+        
+      
+        
+        
+    }
+}
