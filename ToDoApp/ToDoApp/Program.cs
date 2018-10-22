d81964af63ab3dd1d29b0ad66d80861100b56b9f
mfomniga <44165437+mfomniga@users.noreply.github.com>
2018-10-22 16:50:20 +0200

    change

--- ToDoApp/ToDoApp/Program.cs
+++ ToDoApp/ToDoApp/Program.cs
@@ -1,12 +1,68 @@
-﻿using System;
+﻿//using System;
+//using System.Collections.Generic;
+
+
+//namespace ToDoAddCheckDelete
+//{
+//    class Program
+//    {
+//        private static List<string> namen = new List<string>();
+
+//        static void Main(string[] args)
+//        {
+
+
+//            Console.WriteLine("Benutze die Befehle Add, Check und Delete!");
+
+//            add mathias
+//            string userInput = Console.ReadLine();
+
+//            if (userInput.StartsWith("add "))
+//            {
+//                Console.WriteLine("Gib den Namen der Person ein die zu hinzufügen willst:");
+//                string userInputName = Console.ReadLine();
+//                namen.Add(Console.ReadLine());
+
+
+//            }
+//            else if (userInput.StartsWith("delete "))
+//            {
+
+//            }
+
+
+
+
+
+
+//            Console.ReadKey();
+//        }
+//    }
+//}
+
+
+
+
+using System;
+using System.Collections.Generic;
 using ToDoApp.objects;
 
 namespace ToDoApp
 {
     class Program
     {
+        static List<ToDo> member;
+
         static void Main(string[] args)
-        {
+        {            
+            // Zyklus -> App wartet auf Eingabe (z.B. add / check / delete "Todo1")
+
+            // -> App verarbeitet Befehl und gibt Meldung ob´s geklappt hat (Todo1 wurde angelegt ODER ToDo1 exsitiert bereits)
+
+            // gehts von vorne los
+
+
+
             Console.WriteLine("Bitte Namen eingeben");
             string name = Console.ReadLine();
 
@@ -28,6 +84,8 @@ namespace ToDoApp
             ToDo todo = new ToDo(name, done);
 
             Console.ReadLine();
+
+            member = "test";
         }
     }
 }
